db6bbd0af7d07586d2d342f27d60488caa5adbdb
Ladd1993 <aladd1993@gmail.com>
2022-08-05 23:13:31 -0500

    added baseURL

--- src/components/CardPreviewIframe.tsx
+++ src/components/CardPreviewIframe.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { baseUrl } from '../utils/baseURL'
 
 const CardPreviewIframe = ({ id }: { id: string }) => {
     return (
         <div className="bg-black w-[500px] h-[1000px] border border-slate-200">
-            <iframe className='h-full w-full' src={`http://localhost:3000/bup/${id}`} frameBorder="0"></iframe>
+            <iframe className='h-full w-full' src={`${baseUrl}/bup/${id}`} frameBorder="0"></iframe>
         </div>
     )
 }
